8951bad46e845d612b9ff1638d9219753cbf1dac
Galia <g1l4a>
2024-09-25 23:11:30 +0300

    dependency conflicts

--- test/home_screen_golden_test.dart
+++ test/home_screen_golden_test.dart
@@ -2,20 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sleep_tracker/home_screen.dart'; // Import your actual screen
+import 'package:sleep_tracker/home_screen.dart'; 
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUp(() {
-    // Initialize SharedPreferences with mock values
-    SharedPreferences.setMockInitialValues({}); // Provide mock values if necessary
+    SharedPreferences.setMockInitialValues({}); 
   });
 
   testWidgets('Golden test for HomeScreen layout', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: MaterialApp(home: HomeScreen()))); // Pump your widget
+    await tester.pumpWidget(const ProviderScope(child: MaterialApp(home: HomeScreen()))); 
 
-    // Add the golden file comparison
     await expectLater(
       find.byType(HomeScreen),
       matchesGoldenFile('goldens/home_screen.png'),
